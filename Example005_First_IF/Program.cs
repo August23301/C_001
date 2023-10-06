33a68aceb1553c587e656639ee678380102ff156
Anton <baa270799@mail.ru>
2023-10-06 10:50:30 +0500

    fifth commit

---
+++ Example005_First_IF/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Напиши, как тебя зовут))");
+string username = Console.ReadLine();
+if (username.ToLower() == "aнтон"){
+    Console.WriteLine ("О, Привет Тезка!!!");
+}
+    else {
+        Console.WriteLine ("Ну, привет..." + username);}
